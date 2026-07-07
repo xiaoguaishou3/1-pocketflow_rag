22356b48e3ce20f4e373efa58c7b88d7498e82a9
Emily QJ <3063888267@qq.com>
2026-07-07 18:50:44 +0800

    update to MilvusClient

--- Server_Ask/milvus_store/client.py
+++ Server_Ask/milvus_store/client.py
@@ -1,32 +1,30 @@
 """Milvus 连接管理"""
 
-from pymilvus import connections, utility
+from pymilvus import MilvusClient
 from .config import MILVUS_HOST, MILVUS_PORT
 
 
-def connect(host: str = MILVUS_HOST, port: str = MILVUS_PORT) -> None:
-    """连接到 Milvus 服务器"""
-    connections.connect(
+def connect(host: str = MILVUS_HOST, port: str = MILVUS_PORT):
+    """connect Milvus server"""
+    client = MilvusClient(
         alias="default",
         host=host,
         port=port
     )
 
-
-def disconnect() -> None:
-    """断开 Milvus 连接"""
-    connections.disconnect("default")
+    return client
 
 
-def get_server_version() -> str:
-    """获取 Milvus 服务器版本"""
-    return utility.get_server_version()
+def get_milvus_version() -> str:
+    """get Milvus version"""
+    m_client = connect()
+    return m_client.get_server_version()
 
 
 def is_connected() -> bool:
-    """检查是否已连接"""
+    """check connection"""
     try:
-        utility.get_server_version()
+        get_milvus_version()
         return True
     except Exception:
         return False
